28dadd64058128ad848c6ca8ca15b56f651af8f5
ValentinaAsk <49205668+ValentinaAsk@users.noreply.github.com>
2020-04-02 14:43:02 +0300

    исправления

--- homework2/ui/ui/pages/campaign_page.py
+++ homework2/ui/ui/pages/campaign_page.py
@@ -10,10 +10,9 @@ class CampaignPage(BasePage):
         self.find(self.locators.GET_SEGMENTS_PAGE_BUTTON).click()
 
     def get_create_campaign_page(self):
-        time.sleep(3)
-        if self.find(self.locators.CREATE_CAMPAIGN_BUTTON).is_displayed():
+        try:
+            self.find(self.locators.CREATE_CAMPAIGN_BUTTON, timeout=10).is_displayed()
             self.find(self.locators.CREATE_CAMPAIGN_BUTTON).click()
-
-        else:
-            self.find(self.locators.CREATE_FIRST_CAMPAIGN_BUTTON).click()
+        except:
+            self.find(self.locators.CREATE_FIRST_CAMPAIGN_BUTTON, timeout=10).click()
 
